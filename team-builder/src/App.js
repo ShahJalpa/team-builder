0f625cae2b2c63d89dc1054c6fa12dbfea1ac2fc
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-01-26 17:26:28 -0500

    form is ready

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+//import useState
+import React, {useState} from 'react';
+//import Form
+import Form from './Components/Form'
+
+//details that the form will have
+const initialFormDetails = {
+  memberName: "", //TEXT INPUT
+  memberEmail:"", //TEXT INPUT
+  memberRole:"",  //DROPDOWN 
+}
 
 function App() {
-  return (
+  // Give the state variable you just declared a default value.
+  const [teamMembers, setTeamMember] = useState([]); 
+
+  //this wil hold the all values which enterd in the form 
+  //and then set back to initial deatils
+  const [formDetails, setFormDetails] = useState(initialFormDetails);
+  
+  const updateForm = (inputName, inputValue) => {
+    setFormDetails({
+      [inputName]: inputValue
+    });
+  };
+
+  const submitForm = () => {
+    const newTeamMember = {
+      memberName: formDetails.memberName.trim(),
+      memberEmail: formDetails.memberEmail.trim(),
+      memberRole: formDetails.memberRole.trim(),
+    }
+  }
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Team Member App</h1>
+      <Form 
+        details={formDetails}
+        update={updateForm}
+        submit={submitForm}
+      />
     </div>
-  );
+  )
+
+
+
+
+  /*THIS WAS GIVEN*/
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
